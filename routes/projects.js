3f7ffa3fd14ac3e1b42e63fc16096003d79b999b
Karan Basu <karan.basu16@gmail.com>
2020-12-05 18:28:27 +0000

    started adding multer

--- routes/projects.js
+++ routes/projects.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const Project = require('../models/projects')
+const multer = require('multer')
+const upload = multer({ dest: '../uploads/' })
 
 router.get('/', (req, res) => {
   console.log(req.body, 'get')
@@ -10,33 +12,33 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', async (req, res) => {
-  // console.log('POST', req.body)
-  const { name, description, year } = req.body.project
-  const index = await Project.countDocuments()
-  const project = new Project({
-    name,
-    description,
-    year,
-    index
-  })
-  for (piece of req.body.pieces) {
-    const { isMain, imgURL, pieceDescription, price, size, pieceYear, materials, showInProj } = piece
-    const newPiece = {
-      project,
-      isMain,
-      imgURL,
-      description: pieceDescription,
-      price,
-      size,
-      materials,
-      year: pieceYear,
-      index: 0,
-      showInProj
-    }
-    project.pieces.push(newPiece)
-  }
-  await project.save()
-  res.json(project)
+  console.log('POST', req.body)
+  // const { name, description, year } = req.body.project
+  // const index = await Project.countDocuments()
+  // const project = new Project({
+  //   name,
+  //   description,
+  //   year,
+  //   index
+  // })
+  // for (piece of req.body.pieces) {
+  //   const { isMain, imgURL, pieceDescription, price, size, pieceYear, materials, showInProj } = piece
+  //   const newPiece = {
+  //     project,
+  //     isMain,
+  //     imgURL,
+  //     description: pieceDescription,
+  //     price,
+  //     size,
+  //     materials,
+  //     year: pieceYear,
+  //     index: 0,
+  //     showInProj
+  //   }
+  //   project.pieces.push(newPiece)
+  // }
+  // await project.save()
+  // res.json(project)
 })
 
 module.exports = router
