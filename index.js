83c2f590420c8b1807b388c64bb4fd2c7cc37f45
Karan Basu <karan.basu16@gmail.com>
2020-12-17 19:24:48 +0000

    configured connect-history-api-fallback i think

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const cors = require('cors')
+const history = require('connect-history-api-fallback')
 
 const project = require('./routes/projects.js')
 
@@ -29,37 +30,10 @@ db.once('open', () => {
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
+app.use(history())
 
 // routes
 app.use('/api/projects', project)
-
-// const makePiece = async () => {
-//   const piece = new ProjectPiece({
-//     // project: 'hello',
-//     description: 'good pic',
-//     isMain: true,
-//     imgURL: 'www.hello',
-//     price: 300,
-//     size: '20 x 20',
-//     year: 1984
-//   })
-//   await piece.save()
-// }
-
-// makePiece()
-
-// const makeProj = async () => {
-//   const proj = new Project({
-//     name: 'hello',
-//     description: 'good pic',
-//     year: 1984,
-//     index: 0
-//   })
-//   await proj.save()
-//   console.log(proj)
-// }
-
-// makeProj()
 
 const port = process.env.PORT || 5000
 
