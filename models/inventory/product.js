47d140be17759004ce54dd91642586436a150aa0
Karan Basu <karan.basu16@gmail.com>
2021-01-13 17:53:27 +0000

    delete inventory route sorted

--- models/inventory/product.js
+++ models/inventory/product.js
@@ -4,54 +4,54 @@ const Schema = mongoose.Schema
 const opts = { toJSON: { virtuals: true } }
 
 const imgSchema = new Schema(
-  {
-    imgURL: {
-      type: String,
-      required: true
+    {
+        imgURL: {
+            type: String,
+            required: true
+        },
+        filename: {
+            type: String,
+            unique: true,
+            required: true
+        },
+        isMain: {
+            required: true,
+            default: false,
+            type: Boolean
+        }
     },
-    filename: {
-      type: String,
-      unique: true,
-      required: true
-    },
-    isMain: {
-      required: true,
-      default: false,
-      type: Boolean
-    }
-  },
-  opts
+    opts
 )
 
 imgSchema.virtual('thumbnail').get(function () {
-  return this.imgURL.replace('/upload', '/upload/w_250')
+    return this.imgURL.replace('/upload', '/upload/w_250')
 })
 
 const productSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  images: [imgSchema],
-  type: {
-    type: String,
-    enum: ['print', 'piece', 'other'],
-    required: true
-  },
-  onShow: {
-    type: Boolean,
-    required: true,
-    default: true
-  },
-  order: {
-    type: Number,
-    required: true
-  }
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    images: [imgSchema],
+    type: {
+        type: String,
+        enum: ['print', 'piece', 'other'],
+        required: true
+    },
+    onShow: {
+        type: Boolean,
+        required: true,
+        default: true
+    },
+    order: {
+        type: Number
+        // required: true
+    }
 })
 
 module.exports = mongoose.model('Product', productSchema)
--- routes/inventory.js
+++ routes/inventory.js
@@ -8,67 +8,90 @@ const { storage, cloudinary } = require('../cloudinary')
 const upload = multer({ storage })
 
 router.get('/', async (req, res) => {
-  await Particular.find(async (err, particular) => {
-    const opts = [{ path: 'product' }, { path: 'size_price_qty.size', select: 'size' }]
-    const populatedParticulars = await Particular.populate(particular, opts)
-    res.send(populatedParticulars)
-  })
+    await Particular.find(async (err, particular) => {
+        const opts = [{ path: 'product' }, { path: 'size_price_qty.size', select: 'size' }]
+        const populatedParticulars = await Particular.populate(particular, opts)
+        res.send(populatedParticulars)
+    })
 })
 
 router.post('/', upload.array('imgs'), async (req, res) => {
-  const product = JSON.parse(req.body.product)
-  const { name, type, description, sizes } = product
-  const newProduct = new Product({ name, type, description })
-  console.log(newProduct)
+    const product = JSON.parse(req.body.product)
+    const { name, type, description, sizes } = product
+    const newProduct = new Product({ name, type, description })
+    console.log(newProduct)
 
-  // append images
-  for (let i = 0; i < req.files.length; i++) {
-    let image = {}
-    image.imgURL = req.files[i].path
-    image.filename = req.files[i].filename
-    if (i === 0) {
-      image.isMain = true // default is false
+    // append images
+    for (let i = 0; i < req.files.length; i++) {
+        let image = {}
+        image.imgURL = req.files[i].path
+        image.filename = req.files[i].filename
+        if (i === 0) {
+            image.isMain = true // default is false
+        }
+        newProduct.images.push(image)
     }
-    newProduct.images.push(image)
-  }
-  await newProduct.save()
+    await newProduct.save()
 
-  const particular = new Particular({ product: newProduct })
+    const particular = new Particular({ product: newProduct })
 
-  for (const size of sizes) {
-    const currentSize = await Size.findOneAndUpdate(
-      { size: size.size }, // to lowercase in setter
-      { $push: { products: newProduct._id } },
-      { upsert: true, new: true } // if size doesnt exist, creates new document
-    )
-    // for each size (and price), create new particular (product with size, price, quantity)
-    let sizeInfo = {}
-    sizeInfo.size = currentSize
-    sizeInfo.price = parseFloat(size.price)
-    sizeInfo.qty = parseInt(size.qty)
-    size.index ? (sizeInfo.index = parseInt(size.index)) : (sizeInfo.index = 0)
-    particular.size_price_qty.push(sizeInfo)
-  }
-
-  await particular.save()
-
-  particular.populate([{ path: 'product' }, { path: 'size_price_qty.size', select: 'size' }], (err, doc) => {
-    if (err) {
-      return console.log(err)
+    for (const size of sizes) {
+        const currentSize = await Size.findOneAndUpdate(
+            { size: size.size }, // to lowercase in setter
+            { $push: { products: newProduct._id } },
+            { upsert: true, new: true } // if size doesnt exist, creates new document
+        )
+        // for each size (and price), create new particular (product with size, price, quantity)
+        let sizeInfo = {}
+        sizeInfo.size = currentSize
+        sizeInfo.price = parseFloat(size.price)
+        sizeInfo.qty = parseInt(size.qty)
+        size.index ? (sizeInfo.index = parseInt(size.index)) : (sizeInfo.index = 0)
+        particular.size_price_qty.push(sizeInfo)
     }
-    // sends the particular (which is the product with an array of sizes/prices/quantities)
-    res.json(doc)
-  })
+
+    await particular.save()
+
+    particular.populate([{ path: 'product' }, { path: 'size_price_qty.size', select: 'size' }], (err, doc) => {
+        if (err) {
+            return console.log(err)
+        }
+        // sends the particular (which is the product with an array of sizes/prices/quantities)
+        res.json(doc)
+    })
 })
 
 router.put('/:id', upload.array('imgs'), (req, res) => {
-  const particular = JSON.parse(req.body.particular)
-  const sizes = JSON.parse(req.body.sizes)
-  // add any new images to particular.product.images
-  // update product with particular.product
-  // product section done
-  //
-  console.log(particular, sizes)
+    const particular = JSON.parse(req.body.particular)
+    const sizes = JSON.parse(req.body.sizes)
+    // add any new images to particular.product.images
+    // update product with particular.product
+    // product section done
+    //
+    console.log(particular, sizes)
+})
+
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params
+    const particular = await Particular.findByIdAndDelete(id)
+    const product = await Product.findByIdAndDelete(particular.product._id)
+    //  delete product from array of products associated with each size
+    for (const size of particular.size_price_qty) {
+        const sizeId = size.size
+        const foundSize = await Size.findById(sizeId)
+        const newProdArray = foundSize.products.filter(prod => !prod.equals(product._id))
+        // if no more products associateed with size then delete the size
+        if (!newProdArray.length) {
+            await Size.findByIdAndDelete(sizeId)
+        } else {
+            foundSize.products = newProdArray
+            await foundSize.save()
+        }
+        for (const image of product.images) {
+            await cloudinary.uploader.destroy(image.filename)
+        }
+    }
+    res.send(id)
 })
 
 module.exports = router
