cabc21afe9dc639f0fd7165e2c5d54d8d514bb68
Karan Basu <karan.basu16@gmail.com>
2021-02-08 20:47:12 +0000

    sort filter and send exhibitions to all

--- routes/exhibitions.js
+++ routes/exhibitions.js
@@ -2,10 +2,30 @@ const express = require('express')
 const router = express.Router()
 const Exhibition = require('../models/exhibitions')
 
+// callback tto sort by ascending value of index
+const sortAscending = (a, b) => {
+  return a.index - b.index
+}
+
+// sort the projects and images for one theme by index
+const sortOne = exhibition => {
+  exhibition.images.sort(sortAscending)
+  return exhibition
+}
+
+const sortMany = exhibitions => {
+  for (let i = 0; i < exhibitions.length; i++) {
+    sortOne(exhibitions[i])
+  }
+  return exhibitions
+}
+
+// routes
+
 router.get('/', async (req, res) => {
-  // console.log('geto')
-  const exhibitions = await Exhibition.find({}).sort({ index: 1 })
-  res.status(200).json(exhibitions)
+  const exhibitions = await Exhibition.find({ onShow: true }).sort({ index: -1 })
+  const sortedExhibitions = sortMany(exhibitions)
+  res.status(200).json(sortedExhibitions)
 })
 
 module.exports = router
