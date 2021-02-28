dfbdcdfadb29313a79c2ddf12624891a24cef86f
Karan Basu <karan.basu16@gmail.com>
2021-02-28 22:28:01 +0000

    added static folder and prep SPA

--- index.js
+++ index.js
@@ -96,6 +96,17 @@ app.use((err, req, res, next) => {
   res.status(status).send(message)
 })
 
+// Handle production
+if (process.env.NODE_ENV === 'production') {
+  // set the static folder
+  app.use(express.static(__dirname + '/public'))
+
+  // handle SPA
+  app.get(/.*/, (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+  })
+}
+
 const port = process.env.PORT || 5000
 
 app.listen(port, () => {
